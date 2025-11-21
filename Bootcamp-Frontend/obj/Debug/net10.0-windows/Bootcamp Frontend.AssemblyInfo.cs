d656510c5ea2c91a1b278e5fd341bc9b27dc4a24
Tomás Sáez <t.saezi@udd.cl>
2025-11-21 12:07:02 -0300

    #

--- Bootcamp-Frontend/obj/Debug/net10.0-windows/Bootcamp Frontend.AssemblyInfo.cs
+++ Bootcamp-Frontend/obj/Debug/net10.0-windows/Bootcamp Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bootcamp Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+078a5c8a6516b6fe60058af4cf586e84b82dfac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bootcamp Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bootcamp Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
